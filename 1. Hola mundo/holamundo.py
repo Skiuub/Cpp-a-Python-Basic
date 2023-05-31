d731c33ad93166ae3fc889added9a4dede7b1155
Diego Lorca <66433739+Skiuub@users.noreply.github.com>
2023-05-31 13:56:23 -0400

    Update holamundo.py

--- 1. Hola mundo/holamundo.py
+++ 1. Hola mundo/holamundo.py
@@ -1,3 +1,12 @@
-print("Hola mundo")
+# A diferencia de c++ aqui no hace falta empezar inlcuyendo algun import
+# Por otro lado tampoco es necesario partir con una funcion para que nuestro programa funcione
+
+print("Hola mundo") #print es lo mismo que std::cout o printf 
+
+##################################################################
+# IMPORTANTE
+# En Funciones se hara un comentario sobre esto
+##################################################################
+
 
 #ejecutar en terminal: python holamundo.py

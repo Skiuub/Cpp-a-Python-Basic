c126e9360ba494817f9e06b0656b07a76c22563d
Diego Lorca <66433739+Skiuub@users.noreply.github.com>
2023-05-31 13:52:25 -0400

    Update holamundo.cpp

--- 1. Hola mundo/holamundo.cpp
+++ 1. Hola mundo/holamundo.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
-int main () 
+#include <iostream>  //definimos el include basico
+int main ()  //Se define la funcion principal de nuestro archivo
 {
-    std::cout << "Hola, mundo";
-    return 0;
+    std::cout << "Hola, mundo"; //Hacemos un Hola mundo con std::cout o tambien con un printf segun entiendo
+    return 0; //Para terminar la funcion "main()" retornamos un valor 0 a la funcion
 }
 
 // Compilación: g++ holamundo.cpp -o holamundo
